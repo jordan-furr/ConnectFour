862e66f0ed83d0ad0e31b80a00293988ac22daea
Jordan Furr <you@example.com>
2020-04-08 18:39:50 -0400

    add model files and file structure

---
+++ ConnectFour/ConnectFour/Game.swift
@@ -0,0 +1,9 @@
+//
+//  Game.swift
+//  ConnectFour
+//
+//  Created by Jordan Furr on 4/8/20.
+//  Copyright © 2020 Jordan Furr. All rights reserved.
+//
+
+import Foundation
---
+++ ConnectFour/ConnectFour/Move.swift
@@ -0,0 +1,9 @@
+//
+//  Move.swift
+//  ConnectFour
+//
+//  Created by Jordan Furr on 4/8/20.
+//  Copyright © 2020 Jordan Furr. All rights reserved.
+//
+
+import Foundation
